dc438e501bce75d6e4daef3e888cf94f5e883d7c
RANDRIANASOLOFranco <randrianasolofranco@gmail.com>
2019-08-30 09:23:48 +0300

    ajout2

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 var express = require('express');
 var session = require('cookie-session'); // Charge le middlewares de sessions
 var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
-var urlencodedParser = bodyParser.urlencoded({extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var app = express();
 var hostname = 'localhost';
 const port = 8080;
 
 // On utilise les sessions
-app.use(session({secret: 'todotopsecret'}))
+app.use(session({ secret: 'todotopsecret' }))
 
 /* S'il n'y a pas de todolist dans la session,
 on en crée une vide sous forme d'array avant la suite */
-.use(function(req, res, next){
-  if(typeof(req.session.todolist) == 'undefined'){
-    req.session.todolist = [];
-  }
-  next();
+.use(function(req, res, next) {
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
 })
 
 // On affiche la todolist et le formulaire
-.get('/todo', function(req, res){
-  res.render('todo.ejs', {todolist : req.session.todolist});
+.get('/todo', function(req, res) {
+    res.render('todo.ejs', { todolist: req.session.todolist });
 })
 
 //  On ajoute un élément à la todolist
-.post('/todo/ajouter/',urlencodedParser, function(req, res){
-  if(req.body.newtodo != ''){
-    req.session.todolist.push(req.body.newtodo);
-  }
-  res.redirect('/todo');
+.post('/todo/ajouter/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
 })
 
 // Supprime un élément de la todolist
-.get('/todo/suprimer/:id', function(req, res){
-  if(req.params.id != ''){
-    req.session.todolist.splice(req.params.id, 1);
-  }
-  res.redirect('/todo');
+.get('/todo/suprimer/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
 })
 
 //  On redirige vers la todolist si la page demandée n'est pas trouvée
-.use(function(req, res, next){
-  res.redirect('/todo');
+.use(function(req, res, next) {
+    res.redirect('/todo');
 })
 
 .listen(port, hostname, () => {
-    console.log('Server running at https://' + hostname +':'+ port );
+    console.log('Server running at https://' + hostname + ':' + port);
 });
